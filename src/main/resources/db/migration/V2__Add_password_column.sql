a44f77052a9a839ae5b0317741bae7cb1c1002df
felipeoj <felipeoj2000@gmail.com>
2025-08-16 22:41:40 -0300

    chore: clean up V2 migration file

--- src/main/resources/db/migration/V2__Add_password_column.sql
+++ src/main/resources/db/migration/V2__Add_password_column.sql
@@ -1,17 +1,13 @@
--- Adiciona coluna password (permite NULL temporariamente)
 ALTER TABLE users
     ADD COLUMN password VARCHAR(255);
 
--- Define senha temporária para registros existentes
 UPDATE users
 SET password = 'temp12345678'
 WHERE password IS NULL;
 
--- MySQL usa MODIFY COLUMN, não ALTER COLUMN
 ALTER TABLE users
     MODIFY COLUMN password VARCHAR(255) NOT NULL;
 
--- Adiciona constraint de validação
 ALTER TABLE users
     ADD CONSTRAINT check_password_length
         CHECK (LENGTH(TRIM(password)) >= 8);
